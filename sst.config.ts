850e328cf3c64e0e36354451ffc088a9dc7e6dc0
PhorsBeatRmx <phorsbeatrmx0509@gmail.com>
2024-12-22 15:56:21 -1200

    kak

--- sst.config.ts
+++ sst.config.ts
@@ -1,40 +1,15 @@
-import { SSTConfig } from "sst";
-import { NextjsSite } from "sst/constructs";
+/// <reference path="./.sst/platform/config.d.ts" />
 
-export default {
-  config(_input) {
+export default $config({
+  app(input) {
     return {
-      name: "frontend-client-app",
-      region: "us-west-2",
+      name: "frontend-client",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
     };
   },
-  stacks(app) {
-    app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site", {
-        environment: {
-          NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "",
-          NEXT_PUBLIC_AUTH_ENDPOINT:
-            process.env.NEXT_PUBLIC_AUTH_ENDPOINT || "",
-          NEXT_PUBLIC_CORPORATE_ENDPOINT:
-            process.env.NEXT_PUBLIC_CORPORATE_ENDPOINT || "",
-          NEXT_PUBLIC_USER_ENDPOINT:
-            process.env.NEXT_PUBLIC_USER_ENDPOINT || "",
-          NEXT_PUBLIC_JOB_ENDPOINT: process.env.NEXT_PUBLIC_JOB_ENDPOINT || "",
-          NEXT_PUBLIC_COMPANY_ENDPOINT:
-            process.env.NEXT_PUBLIC_COMPANY_ENDPOINT || "",
-          NEXT_PUBLIC_PUSH_NOTIFICATION_ENDPOINT:
-            process.env.NEXT_PUBLIC_PUSH_NOTIFICATION_ENDPOINT || "",
-          NEXT_PUBLIC_CONVERSATION_ENDPOINT:
-            process.env.NEXT_PUBLIC_CONVERSATION_ENDPOINT || "",
-          UPLOADTHING_TOKEN: process.env.UPLOADTHING_TOKEN || "",
-          NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
-            process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "",
-        },
-      });
-
-      stack.addOutputs({
-        SiteUrl: site.url,
-      });
-    });
+  async run() {
+    new sst.aws.Nextjs("MyFrontendClient");
   },
-} satisfies SSTConfig;
+});
